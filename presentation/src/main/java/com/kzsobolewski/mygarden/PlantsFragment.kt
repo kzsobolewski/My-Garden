3a6efa33e7e6217c0d18d7d942958ee6a0fe6417
kzsobolewski <kzsobolewski@gmail.com>
2020-02-24 02:08:03 +0100

    viewpager and tablayout

---
+++ presentation/src/main/java/com/kzsobolewski/mygarden/PlantsFragment.kt
@@ -0,0 +1,25 @@
+package com.kzsobolewski.mygarden
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PlantsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PlantsFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plants, container, false)
+    }
+
+
+}
---
+++ presentation/src/main/java/com/kzsobolewski/mygarden/TabsFragment.kt
@@ -0,0 +1,43 @@
+package com.kzsobolewski.mygarden
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+
+class TabsFragment : Fragment() {
+
+    private lateinit var adapter: TabsPagerAdapter
+    private lateinit var viewPager: ViewPager2
+
+    companion object{
+        fun newInstance() = PlantsFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_tabs, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        adapter = TabsPagerAdapter(this)
+        viewPager = view.findViewById(R.id.main_view_pager)
+        viewPager.adapter = adapter
+
+        val tabLayout: TabLayout = view.findViewById(R.id.main_tab_layout)
+        TabLayoutMediator(tabLayout, viewPager) {tab, position ->
+             tab.text = when(position) {
+                 0 -> getString(R.string.plants_tab)
+                 else -> getString(R.string.search_tab)
+             }
+        }.attach()
+    }
+}
